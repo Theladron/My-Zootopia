25a7d127dadbc3a1d35477a1ab07c017c77a82f1
Theladron <Philwolframm@gmail.com>
2025-02-17 23:26:16 +0100

    improved html classes and indention, added prey category

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,43 +6,47 @@ def load_data(file_path):
         return json.load(handle)
 
 
-def replace_data(animal_data):
+def replace_html(animal_data):
     with open("animals_template.html", "r") as handle:
         data = handle.read()
     new_data = data.replace("            __REPLACE_ANIMALS_INFO__", animal_data)
     return new_data
 
 
-def write_html(data):
+def write_new_html(data):
     with open("animals.html", "w") as handle:
         handle.write(data)
 
 
-def get_animals_data():
-    data = load_data("animals_data.json")
+def serialize_animal(entry):
+
     output = ""
-    for entry in data:
-        output += '<li class="cards__item">\n'
-        name = get_animal_name(entry)
-        if name is not None:
-            output += f'<div class="card__title">{name}</div>\n'
+    output += '<li class="cards__item">\n'
 
-        output += '<p class="card__text">\n'
+    name = get_animal_name(entry)
+    if name is not None:
+        output += f'  <div class="card__title">{name}</div>\n'
 
-        diet = get_animal_diet(entry)
-        if diet is not None:
-            output += f"<strong>Diet:</strong> {diet}<br/>\n"
+    output += '  <div class="card__text">\n'
+    output += "    <ul>\n"
 
-        location = get_animal_location(entry)
-        if location is not None:
-            output += f"<strong>Location:</strong> {location}<br/>\n"
+    diet = get_animal_diet(entry)
+    if diet is not None:
+        output += f"      <li><strong>Diet:</strong> {diet}</li>\n"
 
-        animal_type = get_animal_type(entry)
-        if animal_type is not None:
-            output += f"<strong>Type:</strong> {animal_type}<br/>\n"
+    location = get_animal_location(entry)
+    if location is not None:
+        output += f"      <li><strong>Location:</strong> {location}</li>\n"
 
-        output += "</p></li>\n"
+    animal_type = get_animal_type(entry)
+    if animal_type is not None:
+        output += f"      <li><strong>Type:</strong> {animal_type}</li>\n"
 
+    prey = get_animal_prey(entry)
+    if prey is not None:
+        output += f"      <li><strong>Prey:</strong> {prey}</li>\n"
+
+    output += "    </ul>\n  </div>\n</li>\n"
 
     return output.replace("â€™s ", "&#x27;S ")  # corrects the formatting error happening for ' symbol
 
@@ -63,10 +67,19 @@ def get_animal_type(entry):
     return entry.get("characteristics", {}).get("type")
 
 
+def get_animal_prey(entry):
+    return entry.get("characteristics", {}).get("prey")
+
+
 def main():
-    animal_data = get_animals_data()
-    new_data = replace_data(animal_data)
-    write_html(new_data)
+    data = load_data("animals_data.json")
+    output = ""
+    for animal_obj in data:
+        output += serialize_animal(animal_obj)
+
+    new_data = replace_html(output)
+
+    write_new_html(new_data)
 
 
 if __name__ == "__main__":
