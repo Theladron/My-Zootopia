9b91c972774a29b6389b449ade902e1e207c1cd5
Theladron <Philwolframm@gmail.com>
2025-02-17 23:49:57 +0100

    Added user input for skin types, handled no animals found exception

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,13 +7,16 @@ def load_data(file_path):
 
 
 def replace_html(animal_data):
+
     with open("animals_template.html", "r") as handle:
         data = handle.read()
+
     new_data = data.replace("            __REPLACE_ANIMALS_INFO__", animal_data)
     return new_data
 
 
 def write_new_html(data):
+
     with open("animals.html", "w") as handle:
         handle.write(data)
 
@@ -52,33 +55,64 @@ def serialize_animal(entry):
 
 
 def get_animal_name(entry):
+
     return entry.get("name", {})
 
 
 def get_animal_diet(entry):
+
     return entry.get("characteristics", {}).get("diet")
 
 
 def get_animal_location(entry):
+
     return entry.get("locations", [None])[0]
 
 
 def get_animal_type(entry):
+
     return entry.get("characteristics", {}).get("type")
 
 
 def get_animal_prey(entry):
+
     return entry.get("characteristics", {}).get("prey")
 
 
-def main():
+def create_animal_html(user_input):
+
     data = load_data("animals_data.json")
     output = ""
+
     for animal_obj in data:
-        output += serialize_animal(animal_obj)
+        skin_type = animal_obj.get("characteristics", {}).get("skin_type")
+        if user_input.lower() in skin_type.lower():
+            output += serialize_animal(animal_obj)
 
+    if output == "":
+        return (f'<div class="card__title">No Animals with '
+                f'skin type "{user_input}" found</div>')
+    return output
+
+
+def get_user_input():
+
+    print("""Possible skin types:
+Hair
+Fur
+Scales
+All   for every animal
+
+Enter the skin type of the animals you want to see: """, end="")
+    user_input = input("")
+    return user_input
+
+
+def main():
+
+    user_input = get_user_input()
+    output = create_animal_html(user_input)
     new_data = replace_html(output)
-
     write_new_html(new_data)
 
 
